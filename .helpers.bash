3a370eebfe5305ced6d4401d14dff7e3bdb1786d
jurriaanschrofer <jschrofer@gmail.com>
2021-12-25 20:35:00 +0100

    delete comment

--- .helpers.bash
+++ .helpers.bash
@@ -3,7 +3,3 @@
 # Suppress the annoying Apple messages on a shell boot, which urge you
 # to use zsh instead of bash.
 export BASH_SILENCE_DEPRECATION_WARNING=1
-
-# Define the helpful 'shutup' command, to quickly and aggressively kill
-# servers.
-# alias shutup="cat tmp/pids/server.pid | xargs -n1 -J pid kill -9 pid"
