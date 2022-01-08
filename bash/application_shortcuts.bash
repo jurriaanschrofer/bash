9ddc98313601851bac3d62fdf6f5daffd31cf709
jurriaan_schrofer <jschrofer@gmail.com>
2022-01-08 23:07:59 +0100

    add zsh

--- bash/application_shortcuts.bash
+++ bash/application_shortcuts.bash
@@ -11,34 +11,3 @@ function open_sesame {
     open -a "$app"
   done
 } 
-
-
-
-### TRY 1 TO CLOSE
-
-# function close_sesame {
-#   killall "Sublime Text"
-#   killall "Sublime Merge"
-#   killall "Notion"
-#   killall "Postman"
-#   killall "iTerm"
-# }
-
-
-
-### TRY 2 TO CLOSE
-
-# applications=("Notion" "Postman" "Sublime Text Notion Postman")
-
-# function close_sesame {
-#   for ((i = 0; i < ${#applications[@]}; i++))
-#   do
-#     app="${applications[$i]}"
-#     killall $app
-#   done
-# }
-
-
-### TRY 3 TO CLOSE
-
-# Above loop with in applescript
---
+++ zsh/source_all_scripts.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+project_root=~/code/coding_config
+
+bash $project_root/bash/a_huge_welcome.bash
