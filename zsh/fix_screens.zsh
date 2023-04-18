a5a566db4cb7832ad32a01bfdf97b41234ee8688
Jurr <jschrofer@gmail.com>
2023-04-18 22:40:26 +0200

    add source

---
+++ zsh/fix_screens.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+fix () {
+  displayplacer "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:2056x1329 hz:120 color_depth:8 scaling:on origin:(0,0) degree:0" "id:409A88F3-7238-475F-9F76-D62C28EED653 res:3840x2160 hz:60 color_depth:8 scaling:off origin:(931,-2160) degree:0" "id:57D0029B-75E5-4229-9102-AE92F213171B res:3840x2160 hz:60 color_depth:8 scaling:off origin:(-2909,-2160) degree:0"
+}
--- zsh/source_all_scripts.zsh
+++ zsh/source_all_scripts.zsh
@@ -4,3 +4,4 @@ project_root=~/code/coding_config
 
 bash $project_root/bash/a_huge_welcome.bash
 source $project_root/zsh/clang_helpers.zsh
+source $project_root/zsh/fix_screens.zsh
